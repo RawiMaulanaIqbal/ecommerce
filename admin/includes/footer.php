37cd60113947a388fda0976f765168715d8cd5ca
RawiMaulanaIqbal <rawimaulanaiqbal11@gmail.com>
2021-06-24 21:31:59 +0700

    push

---
+++ admin/includes/footer.php
@@ -0,0 +1,7 @@
+<footer class="main-footer">
+    <div class="pull-right hidden-xs">
+      <b>All rights reserved</b>
+    </div>
+    <strong>Copyright &copy; 2020 Brought To You By:<a href="https://code-projects.org/">FAIS CANDRA
+</a></strong>
+</footer>
